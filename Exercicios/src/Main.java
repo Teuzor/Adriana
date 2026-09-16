a306fb11baa38ff61b8fe93d3f21eaea73400aeb
Teuzo <mateusdanielsantos926@gmail.com>
2026-09-15 22:42:40 -0300

    Exercicios com Interface

--- Exercicios/src/Main.java
+++ Exercicios/src/Main.java
@@ -1,53 +1,51 @@
-public static void main(String[] args) {
+import java.util.Scanner;
 
-    int escolha;
+public class Main {
+    public static void main(String[] args) {
 
-    System.out.println("Informe o exercicio: ");
-    System.out.println("========================== ");
-    Scanner sc = new Scanner(System.in);
+        int escolha;
 
-    System.out.println("1 - Verificar Dívisivel ");
+        System.out.println("Informe o exercicio: ");
+        System.out.println("========================== ");
+        Scanner sc = new Scanner(System.in);
 
-    System.out.println("2 - Inserir Senha ");
+        System.out.println("1 - Verificar Dívisivel ");
 
-    System.out.println("3 - Aplicar Desconto ");
+        System.out.println("2 - Inserir Senha ");
 
-    System.out.println("4 - Temperatura ");
+        System.out.println("3 - Aplicar Desconto ");
 
-    System.out.println("5 - Dividir Camelos ");
+        System.out.println("4 - Temperatura ");
 
-    System.out.println("6 - Custo em Gasolina ");
+        System.out.println("5 - Dividir Camelos ");
 
-    System.out.println("7 - Valor de Aluguel");
+        System.out.println("6 - Custo em Gasolina ");
 
-    escolha = sc.nextInt();
+        System.out.println("7 - Valor de Aluguel");
 
-    if (escolha == 1) {
-        Multiplo.executar();}
+        escolha = sc.nextInt();
 
-    else if (escolha == 2) {
-        Senha.executar();}
+        if (escolha == 1) {
+            Multiplo.executar();
+        } else if (escolha == 2) {
+            Senha.executar();
+        } else if (escolha == 3) {
+            Desconto.executar();
+        } else if (escolha == 4) {
+            Temperatura.executar();
+        } else if (escolha == 5) {
+            DivisaoCamelos.executar();
+        } else if (escolha == 6) {
+            Distancia.executar();
+        } else if (escolha == 7) {
+            Aluguel.executar();
+        } else {
+            System.out.println("Número incondizente");
+        }
 
-    else if (escolha == 3) {
-        Desconto.executar();}
 
-    else if (escolha == 4) {
-        Temperatura.executar();}
-
-    else if (escolha == 5) {
-        DivisaoCamelos.executar();}
-
-    else if (escolha == 6) {
-        Distancia.executar();}
-
-    else if (escolha == 7) {
-        Aluguel.executar();}
-
-    else {
-        System.out.println("Número incondizente");
     }
 
-
-
-
+    void main() {
+    }
 }
